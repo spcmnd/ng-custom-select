0e924bbe5bb4d8a15ec891450f0a6aecfcb01a9d
spcmnd <spcmndd@protonmail.com>
2021-04-05 21:18:22 +0200

    Clean boilerplate and create custom select component

---
+++ src/app/custom-select/custom-select.component.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CustomSelectComponent } from './custom-select.component';
+
+@NgModule({
+  declarations: [CustomSelectComponent],
+  exports: [CustomSelectComponent],
+})
+export class CustomSelectComponentModule {}
---
+++ src/app/custom-select/custom-select.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-select',
+  templateUrl: 'custom-select.component.html',
+  styleUrls: ['custom-select.component.scss'],
+})
+export class CustomSelectComponent {}
